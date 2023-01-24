ade800df822314350496c16885a27ee760a08f95
Dominik <domi.paschinger@hotmail.com>
2023-01-24 16:26:48 +0100

    Everything done, without divide

--- Web-Übungen Abgaben/Mathequiz/script.js
+++ Web-Übungen Abgaben/Mathequiz/script.js
@@ -7,7 +7,7 @@ let gameLevel = 1;
 let levelPassed = false;
 let gameWon = false;
 let numbers = [2];
-
+let life = 3;
 
 const startBtn = document.getElementById("start-btn");
 const question = document.getElementById("question");
@@ -22,58 +22,63 @@ const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 function styling() {
     switch (gameLevel) {
         case 1:
-            if (gameLevel === 1) {
+            gameLevel = 1
                 document.getElementById("Level1").style.borderColor = '#00FFFF';
-            }
             break;
         case 2:
             document.getElementById("Level1").style.borderColor = '#ADFF2F';
-            if (gameLevel === 2) {
+            gameLevel = 2
                 document.getElementById("Level2").style.borderColor = '#00FFFF';
-            }
             break;
         case 3:
             document.getElementById("Level2").style.borderColor = '#ADFF2F';
-            if (gameLevel === 3) {
+            gameLevel = 3
                 document.getElementById("Level3").style.borderColor = '#00FFFF';
-            }
             break;
         case 4:
             document.getElementById("Level3").style.borderColor = '#ADFF2F';
-            if (gameLevel === 4) {
+            gameLevel = 4
                 document.getElementById("Level4").style.borderColor = '#00FFFF';
-            }
             break;
         case 5:
             document.getElementById("Level4").style.borderColor = '#ADFF2F';
-            if (gameLevel === 5) {
+            gameLevel = 5
                 document.getElementById("Level5").style.borderColor = '#00FFFF';
-            }
             break;
         case 6:
             document.getElementById("Level5").style.borderColor = '#ADFF2F';
-            if (gameLevel === 6) {
+            gameLevel = 6
                 document.getElementById("Level6").style.borderColor = '#00FFFF';
-            }
             break;
         case 7:
             document.getElementById("Level6").style.borderColor = '#ADFF2F';
-            if (gameLevel === 7) {
+            gameLevel = 7
                 document.getElementById("Level7").style.borderColor = '#00FFFF';
-            }
             break;
         case 8:
             document.getElementById("Level7").style.borderColor = '#ADFF2F';
-            if (gameLevel === 8) {
+            gameLevel = 8
                 document.getElementById("Level8").style.borderColor = '#00FFFF';
-            }
             break;
         case 9:
             document.getElementById("Level8").style.borderColor = '#ADFF2F';
-            if (gameLevel === 9) {
+            gameLevel = 9
                 document.getElementById("Level9").style.borderColor = '#00FFFF';
-            }
             break;
+    }
+}
+
+function lifeCounter () {
+    if (life === 2) {
+        document.getElementById("Life1").style.display = "none";
+    }
+    if (life === 1) {
+        document.getElementById("Life1").style.display = "none";
+        document.getElementById("Life2").style.display = "none";
+    }
+    if (life === 0) {
+        alert("Du hast verkakt!")
+        window.location.reload();
     }
 }
 
@@ -98,21 +103,14 @@ function styling() {
         //For getting random operator
         let randomOperator = operators[Math.floor(Math.random() * operators.length)];
 
-
         //Wenn es ins Minus geht
         if (randomOperator === "-" && numbers[1] > numbers[0]) {
             numbers[0] = numbers[1];
             numbers[1] = numbers[0];
         }
 
-        if(randomOperator === "/" && numbers[1] > numbers[1]) {
-            numbers[0] = numbers[1];
-            numbers[1] = numbers[0];
-        }
-
         //Solve equation
         let solution = eval(`${numbers[0]}${randomOperator}${numbers[1]}`);
-
 
         //For placing the input at random position
         //(1 for num1, 2 for num2, 3 for operator, anything else(4,5 & 6) for solution)
@@ -156,6 +154,7 @@ submitBtn.addEventListener("click", () => {
         else {
             errorMessage.classList.remove("hide");
             errorMessage.innerHTML = "Your solution is wrong!";
+            life--;
         }
     }
     //If user input is empty
@@ -167,7 +166,6 @@ submitBtn.addEventListener("click", () => {
 
 //Start Game
     startBtn.addEventListener("click", () => {
-
         operatorQuestion = false;
         answerValue = "";
         errorMessage.innerHTML = "";
